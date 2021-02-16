a619e21b06b2853f5799196575ef5974cf99be56
Julen <julenaranzabal28@gmail.com>
2021-02-16 20:22:01 +0100

    InputFragment.kt updated

--- app/src/main/java/com/example/pandemicsocialite/InputFragment.kt
+++ app/src/main/java/com/example/pandemicsocialite/InputFragment.kt
@@ -7,24 +7,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class InputFragment : Fragment() {
 
+    private lateinit var personNumber: Number
+    private lateinit var deskNumber: Number
+    private lateinit var distribute: FloatingActionButton
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_input, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        view.findViewById<FloatingActionButton>(R.id.playButton).setOnClickListener {
+
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
